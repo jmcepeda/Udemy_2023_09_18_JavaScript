87210b83fc7b53702e9e035f6cd2ea9ce2e8da36
José Manuel Cepeda Yáñez <42820707+jmcepeda@users.noreply.github.com>
2023-09-30 18:01:21 +0000

    20230930_20_01

--- 02_03dia_5_02_Problema/myScript.js
+++ 02_03dia_5_02_Problema/myScript.js
@@ -1,5 +1,74 @@
+
 function seleccionaPelicula() {
     let edad=document.getElementById("age");
+    let edadvalue = edad.options[edad.selectedIndex].value
+
     let genero=document.getElementById("gen");
+    let generovalue = genero.options[genero.selectedIndex].value
+
+    console.log("Edad: " + edadvalue + " - Genero: " + generovalue);
+
+    
     let peliculaSeleccionada=document.getElementById("peliculaSeleccionada");
+
+    let combinado=edadvalue+generovalue;
+    console.log("Combinado: " + combinado);
+
+    let pelicula;
+
+    switch (combinado) {
+        case "kidscomedia":
+            pelicula="1";
+            break;
+
+        case "kidsaccion":
+            pelicula="2";
+            break;
+        
+        case "kidstodalaFamilia":
+            pelicula="3";
+            break;
+
+        case "kidscienciaFiccion":
+            pelicula="4";
+            break;
+
+        case "adolescentescomedia":
+            pelicula="5";
+            break;
+
+        case "adolescentesaccion":
+            pelicula="6";
+            break;
+        
+        case "adolescentestodalaFamilia":
+            pelicula="7";
+            break;
+
+        case "adolescentescienciaFiccion":
+            pelicula="8";
+            break;
+        case "adultoscomedia":
+            pelicula="9";
+            break;
+
+        case "adultosaccion":
+            pelicula="10";
+            break;
+        
+        case "adultostodalaFamilia":
+            pelicula="11";
+            break;
+
+        case "adultoscienciaFiccion":
+            pelicula="12";
+            break;
+
+        default:
+            pelicula="Algo no ha funcionado"
+    }
+    
+    peliculaSeleccionada.textContent=pelicula;
+
+    
 }
