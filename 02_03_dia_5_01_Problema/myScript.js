aa2681311c01442e15bd04ec6221134d5084e5fb
José Manuel Cepeda Yáñez <42820707+jmcepeda@users.noreply.github.com>
2023-09-30 16:47:00 +0000

    20230930_18_46

--- 02_03_dia_5_01_Problema/myScript.js
+++ 02_03_dia_5_01_Problema/myScript.js
@@ -1,5 +1,5 @@
 function seleccionaPelicula() {
     let edad=document.getElementById("age");
     let genero=document.getElementById("gen");
-    let peliculaSeleccionada=document.getElementById("peliculaSeleccionada")
+    let peliculaSeleccionada=document.getElementById("peliculaSeleccionada");
 }
