8a84ff74ae1c80e13f0e47d429d01e6d76e94890
José Manuel Cepeda Yáñez <42820707+jmcepeda@users.noreply.github.com>
2023-10-02 09:15:05 +0000

    20231002_11_15

--- 02_03_dia_06_01_Problema/myScripts.js
+++ 02_03_dia_06_01_Problema/myScripts.js
@@ -1,14 +1,20 @@
 function muestraTabla() {
     let numero=Number(document.getElementById("numero").value);
-    let listaOl=document.getElementById("listaResultado");
+    let listaUl=document.getElementById("listaResultado");
     console.log(numero);
-    
+        
+    let first = listaUl.firstElementChild;
+    while (first) {
+        first.remove();
+        first = listaUl.firstElementChild;
+    }
+
     let item;
     for (i=1;i<=10;i++) {
         texto= `${numero} x ${i} = ${numero*i}`;
         console.log(i);
         item=document.createElement("li");
         item.innerHTML=texto;
-        listaOl.appendChild(item);
+        listaUl.appendChild(item);
     }
 }
