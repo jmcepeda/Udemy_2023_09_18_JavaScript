7e02c57c5e3c09c3ba962d76a731e457f63ddddd
José Manuel Cepeda Yáñez <42820707+jmcepeda@users.noreply.github.com>
2023-10-19 05:36:00 +0000

    20231019_0735

--- 02_03_dia_06_04_Tablas_Multiplicar/index.js
+++ 02_03_dia_06_04_Tablas_Multiplicar/index.js
@@ -5,4 +5,14 @@ const app=express();
 
 app.listen(3000);
 
+app.get('/employees',(req, res) => res.send("Obteniendo empleados"));
+
+app.post('/employees',(req, res) => res.send("Creando empleados"));
+
+app.put('/employees',(req, res) => res.send("Actualizando empleados"));
+
+app.delete('/employees',(req, res) => res.send("Eliminando empleados"));
+
 console.log("Hello World");
+
+console.log("Server is running in port 3000"); 
