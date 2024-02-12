ff5cbe7c194029f05dd32bf617e8f4f265305d8c
José Manuel Cepeda Yáñez <42820707+jmcepeda@users.noreply.github.com>
2024-02-12 20:37:55 +0100

    20240212_20_37

--- 01_Curso_Programacion/20240208_TypeScript_Udemy_Nicolas_Schurman/dist/index.js
+++ 01_Curso_Programacion/20240208_TypeScript_Udemy_Nicolas_Schurman/dist/index.js
@@ -3,4 +3,52 @@ let mensaje = "Hola Mundo";
 mensaje = "Chanchito Feliz";
 console.log(mensaje);
 mensaje = "Chao Mundo";
+console.log(mensaje);
+mensaje = "Hola Mundo Feliz";
+console.log(mensaje);
+let extincionDinosaurio = 76000000;
+let dinosaurioFavorito = "T-Rex";
+let extintos = true;
+console.log(extincionDinosaurio);
+console.log(dinosaurioFavorito);
+console.log(extintos);
+const chanchitoFeliz = () => {
+    console.log("Super Chanchito Feliz Function");
+};
+chanchitoFeliz();
+let animales = ["Pera", "Manzana", "Melon"];
+let numbs = [1, 2, 4, 67, 34];
+let checks = [true, false, true];
+let numbs2 = [34, 56, 89, 33];
+const dobleArray = (arrayNum) => {
+    let newArrNum = arrayNum.map(x => x * 2);
+    console.log(newArrNum);
+};
+dobleArray(numbs);
+let tupla = [1, "chanchito", 3, ["Alberto", "Manuel", "Jose"]];
+const chica = "s";
+const mediana = "m";
+const grande = "l";
+const extragrande = "xl";
+var Talla;
+(function (Talla) {
+    Talla[Talla["Chica"] = 0] = "Chica";
+    Talla[Talla["Mediana"] = 1] = "Mediana";
+    Talla[Talla["Grande"] = 2] = "Grande";
+    Talla[Talla["ExtraGrande"] = 3] = "ExtraGrande";
+})(Talla || (Talla = {}));
+var Talla2;
+(function (Talla2) {
+    Talla2[Talla2["Chica"] = 3] = "Chica";
+    Talla2[Talla2["Mediana"] = 4] = "Mediana";
+    Talla2[Talla2["Grande"] = 5] = "Grande";
+    Talla2[Talla2["ExtraGrande"] = 6] = "ExtraGrande";
+})(Talla2 || (Talla2 = {}));
+var Talla3;
+(function (Talla3) {
+    Talla3["Chica"] = "s";
+    Talla3["Mediana"] = "m";
+    Talla3["Grande"] = "l";
+    Talla3["ExtraGrande"] = "xl";
+})(Talla3 || (Talla3 = {}));
 //# sourceMappingURL=index.js.map
--- 01_Curso_Programacion/20240208_TypeScript_Udemy_Nicolas_Schurman/src/index.ts
+++ 01_Curso_Programacion/20240208_TypeScript_Udemy_Nicolas_Schurman/src/index.ts
@@ -4,3 +4,57 @@ mensaje="Chanchito Feliz"
 console.log(mensaje)
 
 mensaje="Chao Mundo"
+
+console.log(mensaje)
+
+mensaje="Hola Mundo Feliz"
+
+console.log(mensaje)
+
+let extincionDinosaurio=76_000_000
+let dinosaurioFavorito="T-Rex"
+let extintos=true
+
+console.log(extincionDinosaurio)
+console.log(dinosaurioFavorito)
+console.log(extintos)
+
+
+const chanchitoFeliz = () => {
+    console.log("Super Chanchito Feliz Function")
+}
+
+chanchitoFeliz()
+
+let animales: string[]=["Pera", "Manzana", "Melon"]
+let numbs: number[]=[1,2,4,67,34]
+let checks: boolean[]=[true, false,true]
+let numbs2: Array<number>=[34,56,89,33]
+
+const dobleArray=(arrayNum: number[]) => {
+    let newArrNum=arrayNum.map(x=> x*2)
+    console.log(newArrNum)
+}
+
+dobleArray(numbs)
+
+// Vamos a hacer tuplas
+
+let tupla: [number, string, number, string[]]=[1, "chanchito",3, ["Alberto", "Manuel", "Jose"]]
+
+// Ahora nos ponemos con las Enumeraciones enums
+
+const chica="s"
+const mediana="m"
+const grande="l"
+const extragrande="xl"
+
+// PascalCase
+enum Talla {Chica, Mediana, Grande, ExtraGrande}
+
+enum Talla2 {Chica=3, Mediana, Grande, ExtraGrande}
+
+enum Talla3 {Chica=chica, Mediana=mediana, Grande=grande, ExtraGrande=extragrande}
+
+
+
