ca5d91430fc07062420ffd988fc63c27493a3300
José Manuel Cepeda Yáñez <42820707+jmcepeda@users.noreply.github.com>
2024-03-01 07:26:53 +0100

    20240301_07_26

--- 01_Curso_Programacion/20240208_TypeScript_Udemy_Nicolas_Schurman/dist/con-js/saludos2.js
+++ 01_Curso_Programacion/20240208_TypeScript_Udemy_Nicolas_Schurman/dist/con-js/saludos2.js
@@ -1,2 +1,9 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.holamundo2 = void 0;
+function holamundo2(mensaje, mensaje2) {
+    console.log(`Toma Rabo de Toro desde archivo de JavaScript: ${mensaje} y :${mensaje2}`);
+    return mensaje;
+}
+exports.holamundo2 = holamundo2;
 //# sourceMappingURL=saludos2.js.map
