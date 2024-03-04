bf83cfc3515fee73f629bea5ec7df48ac72c7d5b
José Manuel Cepeda Yáñez <42820707+jmcepeda@users.noreply.github.com>
2024-03-04 07:26:33 +0100

    20240304_07_26

--- 01_Curso_Programacion/2024020304_TypeScripts_NODE_Udemy_Nicolas_Schurman/index.ts
+++ 01_Curso_Programacion/2024020304_TypeScripts_NODE_Udemy_Nicolas_Schurman/index.ts
@@ -1,6 +1,37 @@
+// Cosas a Hacer en Consola para Instalar Dependencias y Módulos Necesarios
+
+// Para poder ejecutar directamente los archivos de TypeScript sin necesidad de compilarlo, es necesario instalar con "npm" el módulo "ts-node"
+// "npm i -D ts-node"
+
+// Otro aspecto a Hacer, vamos a tener que hacer es instalar en el proyecto "-S" express
+// npm i -S express
+
+// Por último vamos a instalar las definiciones de los tipos
+// Tenemos que buscar la Definición de los Tipos pues no viene con la instalación
+// Cuando instalamaos los módulos no vienen con los tipos para typeScript, dado que estos módulos puede usarse directamente en JavaScript, por tanto hay que instalarlos. Todas estas definiciones las podemos encontrar en "https://github.com/DefinitelyTyped/DefinitelyTyped", pero las podemos instalar desde npm
+
+// npm i -D @types/express @types/express 
+
+// Además es una buena idea instalar TypeScript como modulo de desarrollo para que todos los desarrolladores lo tengan instalado
+// npm i -D typscript
+
+// Para inicializar nuestra configuración con TypeScript "tsc --init"
+
+
 console.log("Rabito")
 
 const holamundo: string="Hola Mundo"
 
-
 console.log("Imprimiendo por Pantalla: ",holamundo)
+
+//Importante tener claro como se importa express
+
+import express from "express"
+
+const app = express()
+
+app.get("/", (req,res)=>{
+    res.send("Hola Perrita")
+})
+const PORT=3000
+app.listen(PORT,() => console.log("El Servidor esta corriendo en el puerto: ", PORT))
